a84df81c70d09b5216604edc05e8d4518002137f
wcf <1533144693@qq.com>
2020-05-17 00:42:18 +0800

    更新了数据库定义

--- docs/script.sql
+++ docs/script.sql
@@ -1,117 +1,99 @@
 create table collage
 (
-    cid  varchar(16) not null
-        constraint collage_pk
-            primary key nonclustered,
-    name varchar(32) not null
-)
-go
+    id   varchar(16) not null,
+    name varchar(32) not null,
+    constraint collage_id_uindex
+        unique (id)
+);
 
-create unique index collage_cid_uindex
-    on collage (cid)
-go
-
-create unique index collage_name_uindex
-    on collage (name)
-go
+alter table collage
+    add primary key (id);
 
 create table health_info
 (
-    uid                 varchar(16) not null
-        constraint health_info_pk
-            primary key nonclustered,
-    tel                 varchar(16) not null,
-    if_danger_14        int         not null,
-    if_abroad_14        int         not null,
-    if_touch_illness_14 int         not null,
-    if_ill              int,
-    health_status       int
-)
-go
+    uid              varchar(12) not null,
+    tel              varchar(16) not null,
+    if_danger_14     int         not null,
+    if_abroad_14     int         not null,
+    if_touch_illness int         not null,
+    if_ill           int         not null,
+    status           int         not null,
+    constraint health_info_uid_uindex
+        unique (uid)
+);
 
-create unique index health_info_uid_uindex
-    on health_info (uid)
-go
+alter table health_info
+    add primary key (uid);
 
 create table major
 (
-    mid  varchar(16) not null
-        constraint major_pk
-            primary key nonclustered
-        constraint major_collage_fk
-            references collage
-            on update cascade on delete cascade,
-    name varchar(32) not null,
-    cid  varchar(16) not null
-)
-go
+    id      varchar(16) not null,
+    name    varchar(32) not null,
+    collage varchar(16) null,
+    constraint major_id_uindex
+        unique (id),
+    constraint major_collage_fk
+        foreign key (collage) references collage (id)
+            on update cascade on delete cascade
+);
+
+alter table major
+    add primary key (id);
 
 create table class
 (
-    class_id varchar(16) not null
-        constraint class_pk
-            primary key nonclustered
-        constraint class_major_fk
-            references major
-            on update cascade on delete cascade,
-    name     varchar(32) not null
-)
-go
+    id    varchar(16) not null,
+    name  varchar(32) null,
+    major varchar(16) null,
+    constraint class_id_uindex
+        unique (id),
+    constraint class_major_fk
+        foreign key (major) references major (id)
+            on update cascade on delete cascade
+);
 
-create unique index class_class_id_uindex
-    on class (class_id)
-go
-
-create unique index major_mid_uindex
-    on major (mid)
-go
+alter table class
+    add primary key (id);
 
 create table punch_record
 (
-    uid         varchar(16) not null,
-    date        date        not null,
-    time        time        not null,
-    health_info int         not null,
-    constraint punch_record_pk
-        primary key (uid, date)
-)
-go
-
-exec sp_addextendedproperty 'MS_Description', '????', 'SCHEMA', 'dbo', 'TABLE', 'punch_record'
-go
+    uid    varchar(16) not null,
+    date   date        not null,
+    time   time        not null,
+    status int         not null,
+    primary key (uid, date)
+);
 
 create table student
 (
-    sid       varchar(16) not null
-        constraint student_pk
-            primary key nonclustered
-        constraint student_class_fk
-            references class
-            on update cascade on delete cascade,
-    person_id varchar(32) not null,
-    name      varchar(64) not null
-)
-go
+    id        varchar(32) not null,
+    name      varchar(64) null,
+    person_id varchar(32) null,
+    class     varchar(16) null,
+    constraint student_id_uindex
+        unique (id),
+    constraint student_class_fk
+        foreign key (class) references class (id)
+            on update cascade on delete cascade
+);
 
-create unique index student_sid_uindex
-    on student (sid)
-go
+alter table student
+    add primary key (id);
 
 create table teacher
 (
-    tid       varchar(16) not null
-        constraint teacher_pk
-            primary key nonclustered
-        constraint collage_teacher_fk
-            references collage
-            on update cascade on delete cascade,
-    person_id varchar(32) not null,
-    name      char(64)
-)
-go
+    id        varchar(16) not null,
+    name      varchar(64) not null,
+    person_id varchar(32) null,
+    collage   varchar(16) null,
+    constraint teacher_id_uindex
+        unique (id),
+    constraint teacher_collage_fk
+        foreign key (collage) references collage (id)
+            on update cascade on delete cascade
+);
 
-create unique index teacher_tid_uindex
-    on teacher (tid)
-go
+alter table teacher
+    add primary key (id);
 
 
